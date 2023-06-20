66df1444efe344cffa65387c972000994978cd23
creative-account <102407879+creative-account@users.noreply.github.com>
2023-06-20 14:45:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var timer = document.getElementById("timer");
+var audio = new Audio("aegigoe.mp4");
+var residue = 0;
+
+function up() {
+    residue += 60;
+    timer.textContent = residue + "秒";
+}
+
+function down() {
+    residue -= 60;
+    if (residue <= 0) {
+        residue = 0;
+    }
+    timer.textContent = residue + "秒";
+}
+
+function count() {
+    residue--;
+    if (residue == 0) {
+        audio.play();
+    }
+    timer.textContent = residue + "秒";
+}
+
+function start() {
+    setInterval(count, 1000);
+}
+
+function fake_stop() {
+    var fake = document.getElementById("fake");
+    fake.remove();
+}
